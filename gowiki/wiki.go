0f6876d3a8c3869394211491d3d9f674f768d20e
Ephraim Kunz <ephraimkunz@me.com>
2017-12-11 15:54:03 -0700

    Create wiki in Go

---
+++ gowiki/wiki.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"errors"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+const dataDirectory = "data/"
+const templateDirectory = "tmpl/"
+
+var templates = template.Must(template.ParseFiles(
+	templateDirectory+"edit.html",
+	templateDirectory+"view.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func (p *Page) save() error {
+	filename := dataDirectory + p.Title + ".txt"
+	err := ioutil.WriteFile(filename, p.Body, 0600) // Read and write by current user
+	return err
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := dataDirectory + title + ".txt"
+	data, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		return nil, err
+	}
+	return &Page{title, data}, nil
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl, p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("Invalid Page Title")
+	}
+	return m[2], nil
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	page, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+
+	renderTemplate(w, "view.html", page)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	page, err := loadPage(title)
+	if err != nil {
+		page = &Page{Title: title}
+	}
+
+	renderTemplate(w, "edit.html", page)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+	p := &Page{title, []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func main() {
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+	http.ListenAndServe(":8080", nil)
+}
---
+++ gowiki/wiki_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_saveAndLoad(t *testing.T) {
+	t.Run("Test that load and store works", func(t *testing.T) {
+		page := &Page{"TestPage", []byte("this is a test")}
+
+		err := page.save()
+		if err != nil {
+			t.Errorf("Error saving test page: %v", err)
+		}
+
+		pageLoaded, err := loadPage(page.Title)
+		if err != nil || !reflect.DeepEqual(page, pageLoaded) {
+			t.Errorf("Error: %v\nSaved: %v\nRead: %v", page, pageLoaded)
+		}
+	})
+}
+
+func Test_loadPage(t *testing.T) {
+	type args struct {
+		title string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *Page
+		wantErr bool
+	}{
+		{"Load page that doesn't exist", args{"TestPage2"}, nil, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := loadPage(tt.args.title)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("loadPage() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("loadPage() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
