d160a78b0f692f0a46f44d65f9f17d1c357c8951
Ephraim Kunz <ephraimkunz@me.com>
2017-12-15 09:36:00 -0700

    Beautify some html / js

--- chat/public/app.js
+++ chat/public/app.js
@@ -8,21 +8,24 @@ new Vue({
         email: null, // Email address used for grabbing an avatar
         username: null, // Our username
         joined: false // True if email and username have been filled in
-    },  created: function() {
+    },
+    created: function () {
         var self = this;
         this.ws = new WebSocket('ws://' + window.location.host + '/ws');
-        this.ws.addEventListener('message', function(e) {
+        this.ws.addEventListener('message', function (e) {
             var msg = JSON.parse(e.data);
-            self.chatContent += '<div class="chip">'
-                    + '<img src="' + self.gravatarURL(msg.email) + '">' // Avatar
-                    + msg.username
-                + '</div>'
-                + emojione.toImage(msg.message) + '<br/>'; // Parse emojis
+            self.chatContent += '<div class="chip">' +
+                '<img src="' + self.gravatarURL(msg.email) + '">' // Avatar
+                +
+                msg.username +
+                '</div>' +
+                emojione.toImage(msg.message) + '<br/>'; // Parse emojis
 
             var element = document.getElementById('chat-messages');
             element.scrollTop = element.scrollHeight; // Auto scroll to the bottom
         });
-    }, methods: {
+    },
+    methods: {
         send: function () {
             if (this.newMsg != '') {
                 this.ws.send(
@@ -30,11 +33,11 @@ new Vue({
                         email: this.email,
                         username: this.username,
                         message: $('<p>').html(this.newMsg).text() // Strip out html
-                    }
-                ));
+                    }));
                 this.newMsg = ''; // Reset newMsg
             }
-        }, join: function () {
+        },
+        join: function () {
             if (!this.email) {
                 Materialize.toast('You must enter an email', 2000);
                 return
@@ -46,7 +49,8 @@ new Vue({
             this.email = $('<p>').html(this.email).text();
             this.username = $('<p>').html(this.username).text();
             this.joined = true;
-        }, gravatarURL: function(email) {
+        },
+        gravatarURL: function (email) {
             return 'http://www.gravatar.com/avatar/' + CryptoJS.MD5(email);
         }
     }
